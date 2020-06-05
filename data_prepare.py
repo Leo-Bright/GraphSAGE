3bb4e59bc3aa075047dae70526091bbb406dfd90
Leo Bright <564312023@qq.com>
2020-06-05 16:24:59 +0800

    graphSAGE可运行版本

--- data_prepare.py
+++ data_prepare.py
@@ -1,5 +1,8 @@
 import json
 import numpy as np
+import random
+from eval_scripts import graph
+from graphsage import utils
 
 
 # supports emb file and tag file
@@ -64,6 +67,45 @@ def gen_node_class_json(node2emb, node2tag, output_json_path):
         json.dump(node2class, f)
 
 
+def gen_graph_json(network, node2idx, feats, node2class, output_json_path):
+
+    result_graph = {'directed': False, 'graph': {'name': 'disjoint_union( ,  )'}, 'nodes': [], 'links': [], 'multigraph': False}
+
+    graph_size = len(network.nodes())
+    test_size = graph_size // 10
+    val_size = graph_size // 5
+
+    links = []
+    nodes = [None for x in range(0, graph_size)]
+
+    for src in network.nodes():
+
+        src_idx = node2idx[src]
+        node_feats = feats[src_idx].tolist()
+        node_class = node2class[src]
+        rd = random.randint(0, graph_size) + 1
+
+        to_test = to_valid = False
+        if rd < test_size:
+            to_test = True
+        elif rd < val_size:
+            to_valid = True
+
+        node = {'id': src, 'feature': node_feats, 'label': node_class, 'test': to_test, 'val': to_valid}
+        nodes[src_idx] = node
+
+        targets = network[src]
+        for targ in targets:
+            targ_idx = node2idx[targ]
+            link = {'test_removed': False, 'train_removed': False, 'target': targ_idx, 'source': src_idx}
+            links.append(link)
+
+    result_graph['nodes'] = nodes
+    result_graph['links'] = links
+
+    json.dump(result_graph, open(output_json_path, 'w+'))
+
+
 if __name__ == '__main__':
 
     node_emb_dict = trans_input_file_to_ndarray('sanfrancisco/embeddings/sanfrancisco_raw_feature_none.embeddings')
@@ -85,6 +127,17 @@ if __name__ == '__main__':
     # node_class_json_path = 'sanfrancisco/train_data/sanfrancisco-class_map.json'
     # gen_node_class_json(node_emb_dict, node_tag_dict, node_class_json_path)
 
-    node2class = json.load(open('sanfrancisco/train_data/sanfrancisco-class_map.json'))
+    example_graph = json.load(open('example_data/toy-ppi-G.json'))
+
+    # step 0-4: generate G.json file
+    network = graph.load_edgelist('sanfrancisco/osm_data/sf_roadnetwork')['graph']
+    node_graph_json_path = 'sanfrancisco/train_data/sanfrancisco-G.json'
+    node_idx_dict = json.load(open('sanfrancisco/train_data/sanfrancisco-id_map.json'))
+    node_feats_ndarray = np.load('sanfrancisco/train_data/sanfrancisco-feats.npy')
+    node_class_dict = json.load(open('sanfrancisco/train_data/sanfrancisco-class_map.json'))
+    gen_graph_json(network, node_idx_dict, node_feats_ndarray, node_class_dict, node_graph_json_path)
+
+    # step 0-5: generate walks.txt file
+
 
     print('done')
